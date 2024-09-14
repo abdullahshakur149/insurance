f3fc1052d6b95ce8caf0ac35da9bac6eb88f59d9
abdullahshakur <abdullahshakur95@gmail.com>
2024-09-14 16:15:42 +0500

    all files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html', // Look for all HTML files in the 'insurance' directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
