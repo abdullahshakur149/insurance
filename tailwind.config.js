1c1bb3fa076ad1ea685610181c42562506aeb3fc
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2024-09-16 23:16:15 +0500

    add the animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './**/*.html', // Look for all HTML files in the 'insurance' directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        fontcolor:"#22227c"
+      },
+      fontFamily:{
+        bebas: ['"Bebas Neue"', 'sans-serif'],
+        futuraAlt: ['Montserrat', 'sans-serif'], // Montserrat as an alternative to Futura
+        // Include Bebas Neue as a custom font
+      }
+    },
   },
   plugins: [],
 }
